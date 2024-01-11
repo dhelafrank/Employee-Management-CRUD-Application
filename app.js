38560186bbebb648703cc0b57f18786cd4a32f50
dhelafrank <ubongabasijerome8@gmail.com>
2024-01-11 17:37:56 +0100

    Renamed Index Route to views to denote rendering

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const {connectToDatabase} = require("./database")
 
-var indexRouter = require('./routes/index');
+var viewRouter = require('./routes/views');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', viewRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler

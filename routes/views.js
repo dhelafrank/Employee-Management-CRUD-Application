d4605295b39557d498811e4d9c05177e7778d601
dhelafrank <ubongabasijerome8@gmail.com>
2024-01-12 01:39:36 +0100

    New: Linked Home Page

--- routes/views.js
+++ routes/views.js
@@ -2,9 +2,17 @@ var express = require('express');
 var router = express.Router();
 require('dotenv').config()
 
-/* GET home page. */
+/* GET auth page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
+    organisation: process.env.ORGANISATION_NAME,
+    projectName:process.env.PROJECT_NAME
+  });
+});
+
+// GET home page
+router.get('/home', function (req, res, next) {
+  res.render('home', {
     organisation: process.env.ORGANISATION_NAME,
     projectName:process.env.PROJECT_NAME
   });

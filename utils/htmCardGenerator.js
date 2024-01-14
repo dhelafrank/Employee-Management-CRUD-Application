45284acba53608b0adf24ecdc186af9ca26c7cb3
dhelafrank <ubongabasijerome8@gmail.com>
2024-01-14 18:44:53 +0100

    Fix: Overflowed Contents UI

--- utils/htmCardGenerator.js
+++ utils/htmCardGenerator.js
@@ -1,12 +1,11 @@
 function cardGenerator(cardClassName, inputObject) {
     let htmlContent = ``
     let htmlTemplate = (data) => {
-        console.log(data.department);
         return `
                  <li class="${cardClassName}">
                      <div>
                          <p class="name">${data.firstName || data.name} ${data.lastName || ""}</p>
-                         <p>${data.department ||`No. of Employees: ${data.noOfEmployees}`}</p>
+                         <p>${data.departments ||`No. of Employees: ${data.noOfEmployees}`}</p>
                      </div>
                  </li>
                 `
